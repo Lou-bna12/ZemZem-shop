094cd745b07ef82aa9c0887056685d1997c26d43
Loubna_Sellam <loubnasellam49@gmail.com>
2023-09-27 17:20:00 +0200

    git commit

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ const data = {
         {
             name: 'Nike Slim Tshirt',
             slug: 'nike-slim-tshirt',
-            category: 'Vêtements Hommes',
+            category: 'T-shirt',
             image:'/images/p1.jpg',
             price: 120,
             countInStock: 100,
@@ -14,4 +14,45 @@ const data = {
 
         },
 
-    
+        {
+            name: 'Adidas Fit T-Shirt',
+            slug: 'adidas-fit-t-shirt',
+            category: 'T-shirt',
+            image: '/images/p2.jpg',
+            price: 50,
+            countInStock: 220,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 't-shirt de haute qualité',
+
+        },
+
+        {
+            name: 'Nike Slim Pant',
+            slug: 'nike-slim-pant',
+            category: 'Pantalons',
+            image: '/images/p3.jpg',
+            price: 65,
+            countInStock: 150,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'pontalon de haute qualité',
+        },
+        {
+            name: 'Adidas Fit Pant',
+            slug: 'adidas-fit-pant',
+            category: 'Pantalons',
+            image: '/images/p4.jpg',
+            price: 65,
+            countInStock: 500,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'pontalon de haute qualité',
+        }    
+    ],
+};
+
+export default data; 
